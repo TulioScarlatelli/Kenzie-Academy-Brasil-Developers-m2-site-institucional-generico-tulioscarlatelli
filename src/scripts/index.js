cd23758e0436c0c0d60e241c9efd44d890901f40
Túlio Scarlatelli <scarlatellitulio@gmail.com>
2025-01-02 09:41:59 -0300

    Tarefa 5 concluída

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,21 @@
-function mostrarModal () {
-    const button = document.getElementById("#showModal")
-    const modalContainer = document.querySelector(".modal__register")
+function handleModal () {
+    const button1 = document.querySelector(".header__button");
+    const button2 = document.querySelector(".faq__left__button");
+    const modalContainer = document.querySelector(".modal__container");
+    const buttonClose = document.querySelector(".modal__register__button--close")
 
-    button.addEventListener('click', () => {
-        modalContainer.showModal()
-    })
+
+    button1.addEventListener('click', () => {
+        modalContainer.showModal();
+    });
+
+    button2.addEventListener('click', () => {
+        modalContainer.showModal();
+    });
+
+    buttonClose.addEventListener('click', () => {
+        modalContainer.close();
+    });
 }
 
-
-
-mostrarModal()
+handleModal();
